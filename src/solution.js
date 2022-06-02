cd1f3258cf334694246f809f89713e56d9360d70
Alexandra Vysochina <puzinka_77@mail.ru>
2022-06-02 20:27:00 +0300

    Created file solution.js

---
+++ src/solution.js
@@ -0,0 +1,36 @@
+// Список курсов
+let courses = [
+    { name: "Courses in England", prices: [0, 100] }, 
+    { name: "Courses in Germany", prices: [500, null] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+];
+
+// Варианты цен (фильтры), которые ищет пользователь
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+// Отфильтрованные курсы
+const selectionCourses1 = (course) => {
+    requiredRange1 = [null, 200];
+    return filter(requiredRange1, course);
+}
+
+const selectionCourses2 = (course) => {
+    requiredRange2 = [100, 350];
+    return filter(requiredRange2, course);
+}
+
+const selectionCourses3 = (course) => {
+    requiredRange3 = [200, null];
+    return filter(requiredRange3, course);
+}
+
+console.log('requiredRange1 = [null, 200]:\n', courses.filter(selectionCourses1));
+console.log('requiredRange2 = [100, 350]:\n', courses.filter(selectionCourses2));
+console.log('requiredRange3 = [200, null]:\n', courses.filter(selectionCourses3));
